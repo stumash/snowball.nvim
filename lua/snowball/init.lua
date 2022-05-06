74296ab71825743c7d0eaa7ed1755598ef232683
Stuart Mashaal <stuart.mashaal@gmail.com>
2022-05-06 11:27:23 -0400

    expose `reverse_scrollbar`

--- lua/snowball/init.lua
+++ lua/snowball/init.lua
@@ -10,5 +10,6 @@ M.provider_name = config.provider_name
 
 M.provider = utils.whitespace_provider
 M.add_whitespace_component = utils.add_whitespace_component
+M.reverse_scrollbar = utils.reverse_scrollbar
 
 return M
